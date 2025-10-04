4bf8a58496e3edf2b9865e0ae22ca6ab65315665
SenseiRoa <andreoo_124@hotmail.com>
2025-10-04 14:46:00 -0500

    feat: Remove outdated instructions and route map documents; update API service formatting

--- employee-register/src/app/services/api.service.ts
+++ employee-register/src/app/services/api.service.ts
@@ -2,10 +2,10 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class ApiService {
   private base = environment.apiBaseUrl;
-  token = "sub=user_everglades,exp=1759469195.a_very_insecure_secret_key";
+  token = "sub=user_everglades,exp=1759500992.a_very_insecure_secret_key";
   headers = {};
 
   constructor(private http: HttpClient) {
@@ -13,14 +13,24 @@ export class ApiService {
   }
 
   registerFace(name: string, image_base64: string) {
-    return this.http.post(`${this.base}/employees/register_face`, { name, image_base64 }, { headers: this.headers });
+    return this.http.post(
+      `${this.base}/employees/register_face`,
+      { name, image_base64 },
+      { headers: this.headers }
+    );
   }
 
   checkInOut(image_base64: string) {
-    return this.http.post(`${this.base}/employees/check_in_out`, { image_base64 }, { headers: this.headers });
+    return this.http.post(
+      `${this.base}/employees/check_in_out`,
+      { image_base64 },
+      { headers: this.headers }
+    );
   }
 
   getLogs() {
-    return this.http.get(`${this.base}/employees/logs`, { headers: this.headers });
+    return this.http.get(`${this.base}/employees/logs`, {
+      headers: this.headers
+    });
   }
 }
